6c005df1842cb4e6ad1a9d57bcb53c7ab9a302d9
Lucas S Lima <lucassilva996@outlook.com>
2020-08-04 11:17:27 -0300

    Upload

---
+++ ExemploRefit/ICepApiService.cs
@@ -0,0 +1,12 @@
+﻿using ExemploRefit;
+using Refit;
+using System.Threading.Tasks;
+
+namespace RefitCepExample
+{
+    public interface ICepApiService
+    {
+        [Get("/ws/{cep}/json")]
+        Task<CepResponse> GetAddressAsync(string cep);
+    }
+}
---
+++ ExemploRefit/Program.cs
@@ -0,0 +1,34 @@
+﻿using Refit;
+using RefitCepExample;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploRefit
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var cepClient = RestService.For<ICepApiService>("http://viacep.com.br");
+                Console.WriteLine("Informe seu CEP");
+
+                string cepInformado = Console.ReadLine().ToString();
+                Console.WriteLine("Consultando informacoes do CEP: " + cepInformado);
+
+                var address = await cepClient.GetAddressAsync(cepInformado);
+
+                Console.Write($"\nLogradouro:{address.Logradouro}, \nBairro:{address.Bairro}, \nCidade:{address.Localidade}, \nUF:{address.Uf}, \nUnidade:{address.Unidade}, \nCódigo IBGE:{address.Ibge}, \nCódigo Gia:{address.Gia}");
+                Console.ReadKey();
+
+            } catch (Exception e)
+            {
+                Console.WriteLine("Erro na chamada da API" + e.Message);
+            }
+        }
+    }
+}
